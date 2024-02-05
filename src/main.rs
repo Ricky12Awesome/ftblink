4bd3902ecfa67f1cd711ccce19f2ef918ffe5054
Ricky12Awesome <29931568+Ricky12Awesome@users.noreply.github.com>
2024-02-04 22:10:09 -0600

    stuffs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,8 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
   let ui = AppWindow::new()?;
 
-  ui.on_request_increase_value({
-    let ui_handle = ui.as_weak();
-    move || {
-      let ui = ui_handle.unwrap();
-      ui.set_counter(ui.get_counter() + 1);
-    }
+  ui.on_input_accepted(move |s| {
+    println!("{s}");
   });
 
   ui.run()
